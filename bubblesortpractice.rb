58dcf9dc8c638696823c27fda2fae9ddc26be5b8
Nori Terada <noriterada19@gmail.com>
2024-01-24 23:29:14 -0700

    finished

--- bubblesortpractice.rb
+++ bubblesortpractice.rb
@@ -2,12 +2,22 @@
 test = [4,3,78,2,0,2]
 
 
-def bubble_sort (arr)
-    arr.each do |num|
+def bubble_sort arr
+    num = arr.length
+
+    loop do
         
+        sorted = false
 
+        (num-1).times do |i|
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i+1], arr[i]
+                sorted = true
+            end
+        end
+        break if not sorted
     end
-
+    puts(arr)
 end
 
 
